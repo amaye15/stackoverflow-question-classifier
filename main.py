b8a918503727649a4e035ec32e53f911146d3fb3
amaye15 <70704207+amaye15@users.noreply.github.com>
2023-11-27 15:40:22 +0000

    weird thing with labels

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ if generate:
 
     input_text = sample["Title"].values[0]
 
-    candidate_labels = [*sample["Label"].values.tolist(), *np.random.choice(list(df["Label"].value_counts().to_dict().keys())[:10], size=5).tolist()]
+    candidate_labels = [*sample["Label"].values.tolist(), *list(df["Label"].value_counts().to_dict().keys())[:10]]
 
     # Initialize the zero-shot classification pipeline
     zero_shot_pipeline = ZeroShotClassificationPipeline("amaye15/Stack-Overflow-Zero-Shot-Classification", st.secrets.Authorization)
