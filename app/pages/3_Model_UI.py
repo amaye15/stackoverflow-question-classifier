b7754e3900700dee6b67dbca3ba5fe63ec39a812
amaye15 <70704207+amaye15@users.noreply.github.com>
2023-12-19 12:41:38 +0000

    remove Generate

--- app/pages/3_Model_UI.py
+++ app/pages/3_Model_UI.py
@@ -12,7 +12,7 @@ generate_title()
 st.markdown("## Model UI")
 
 # User input for the text
-input_text = st.text_area("Write your question here:", "")
+input_text = st.text_area("Write your question here:", "How to loop over a list?")
 
 # User input for the labels, separated by commas
 input_labels = st.text_input("Add your programming languages here:", "python, javascript, java, c#, android, c++, php, git, ios, html")
@@ -65,39 +65,39 @@ if classify:
         st.plotly_chart(fig)
 
 # Button to perform classification
-if generate:
-    df = pd.read_pickle("data/StackOverFlow.pkl.gz")
-    df["Label"] = df.Tags.str.split(",").apply(lambda x: x[0])
-    sample = df.sample(1)
-    input_text = sample["Title"].values[0]
-    top_labels = list(df["Label"].value_counts().to_dict().keys())[:8]
-    candidate_labels = list(set([*sample["Label"].values.tolist(), *top_labels]))
+# if generate:
+#     df = pd.read_pickle("data/StackOverFlow.pkl.gz")
+#     df["Label"] = df.Tags.str.split(",").apply(lambda x: x[0])
+#     sample = df.sample(1)
+#     input_text = sample["Title"].values[0]
+#     top_labels = list(df["Label"].value_counts().to_dict().keys())[:8]
+#     candidate_labels = list(set([*sample["Label"].values.tolist(), *top_labels]))
 
-    # Display a loading message and make the API request
-    with st.spinner('Checking model status...'):
-        result, should_wait = classify_text(zero_shot_pipeline, input_text, candidate_labels)
+#     # Display a loading message and make the API request
+#     with st.spinner('Checking model status...'):
+#         result, should_wait = classify_text(zero_shot_pipeline, input_text, candidate_labels)
     
-    # If the model was loading, wait for the estimated time and then retry
-    while should_wait:
-        with st.spinner(f'Model is loading, please wait... Estimated time: {result["estimated_time"]:.2f} seconds'):
-            time.sleep(result["estimated_time"])  # Wait for the estimated time
-            result, should_wait = classify_text(zero_shot_pipeline, input_text, candidate_labels)
-    # If there's no error, proceed to display results
-    if not should_wait and 'labels' in result and 'scores' in result:
-        # Extract the labels and scores
-        labels = result['labels']
-        scores = result['scores']
-        labels.reverse()
-        scores.reverse()
+#     # If the model was loading, wait for the estimated time and then retry
+#     while should_wait:
+#         with st.spinner(f'Model is loading, please wait... Estimated time: {result["estimated_time"]:.2f} seconds'):
+#             time.sleep(result["estimated_time"])  # Wait for the estimated time
+#             result, should_wait = classify_text(zero_shot_pipeline, input_text, candidate_labels)
+#     # If there's no error, proceed to display results
+#     if not should_wait and 'labels' in result and 'scores' in result:
+#         # Extract the labels and scores
+#         labels = result['labels']
+#         scores = result['scores']
+#         labels.reverse()
+#         scores.reverse()
 
-        # Create a Plotly bar plot
-        fig = px.bar(x=scores, 
-                     y=labels, 
-                     orientation='h', 
-                     labels={'x':'Score (%)', 'y':'Label'}, 
-                     title= f"{sample['Label'].values[0]} - {input_text}", 
-                     )
-        fig.update_layout(xaxis_title='Score', yaxis_title='Label')
+#         # Create a Plotly bar plot
+#         fig = px.bar(x=scores, 
+#                      y=labels, 
+#                      orientation='h', 
+#                      labels={'x':'Score (%)', 'y':'Label'}, 
+#                      title= f"{sample['Label'].values[0]} - {input_text}", 
+#                      )
+#         fig.update_layout(xaxis_title='Score', yaxis_title='Label')
 
-        # Display the Plotly bar plot
-        st.plotly_chart(fig)
+#         # Display the Plotly bar plot
+#         st.plotly_chart(fig)
